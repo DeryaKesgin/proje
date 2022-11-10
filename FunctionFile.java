48bf35da95349c9941a1fd61c88d906924628d7b
DeryaKesgin <99399615+DeryaKesgin@users.noreply.github.com>
2022-11-10 19:19:26 +0300

    Create FunctionFile.java

---
+++ FunctionFile.java
@@ -0,0 +1,92 @@
+package undoredo;
+
+import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Function_File {
+    GUİ gui;
+    String fileName;
+    String fileAdress;
+    
+    public Function_File(GUİ gui) {
+    	this.gui=gui;
+    }
+
+    public void newFile() {
+    	gui.textArea.setText(" ");
+    	gui.window.setTitle("New");
+    	fileName=null;
+    	fileAdress=null;
+    }
+    
+    public void open() {
+    	FileDialog fd = new FileDialog(gui.window,"Open",FileDialog.LOAD);
+    	fd.setVisible(true);
+    	
+    	if(fd.getFile()!=null) {
+    		fileName=fd.getFile();
+    		fileAdress=fd.getDirectory();
+    		gui.window.setTitle(fileName);
+    	}
+    	
+    	try {
+    		BufferedReader br=new BufferedReader(new FileReader(fileAdress+fileName));
+    
+    		gui.textArea.setText("");
+    		
+    		String line=null;
+    		while((line=br.readLine())!=null) {
+    			gui.textArea.append(line+"\n");
+    		}
+    		br.close();
+    	
+    }catch(Exception e) {
+    	System.out.println("FİLE NOT OPENED!");
+    	}
+    }
+
+    public void save() {
+    	
+    	if(fileName==null) {
+    		saveAs();
+    	}
+    	else {
+    		try {
+    			FileWriter fw=new FileWriter(fileAdress+fileName);
+        		fw.write(gui.textArea.getText());
+        		gui.window.setTitle(fileName);
+        		fw.close();
+    			
+    		}catch(Exception e) {
+        		System.out.println("SOMETHİNG WRONG!");
+    		}
+    	}
+    	
+    }
+    
+    public void saveAs() {
+    	FileDialog fd = new FileDialog(gui.window,"Save",FileDialog.SAVE);
+    	fd.setVisible(true);
+    	
+    	if(fd.getFile()!=null) {
+    		fileName=fd.getFile();
+    		fileAdress=fd.getDirectory();
+    		gui.window.setTitle(fileName);
+    	}
+    	try {
+    		FileWriter fw=new FileWriter(fileAdress+fileName);
+    		fw.write(gui.textArea.getText());
+    		fw.close();
+    	}catch(Exception e) {
+    		System.out.println("SOMETHİNG WRONG!");
+    	}
+    }
+    
+   public void exit() {
+	   System.exit(0);
+   } 
+    
+    
+}
